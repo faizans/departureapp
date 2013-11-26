fb6cbefd9c1442cb75b27fbd955f3895e08eb361
yves.lauber@varian.com <yves.lauber@varian.com>
2013-11-26 17:24:29 +0100

    added departureApp

---
+++ src/departureapp/DepartureApp.java
@@ -0,0 +1,15 @@
+package departureapp;
+
+import com.alee.laf.WebLookAndFeel;
+
+public class DepartureApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		WebLookAndFeel.install();	//Set custom look and feel -->http://weblookandfeel.com/
+		//gui zusammenstellen hier ...
+	}
+
+}
