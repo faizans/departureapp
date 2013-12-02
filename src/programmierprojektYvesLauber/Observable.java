188ba3d04b9f53deb0e96d14c8072a918b6c2a36
yves.lauber@varian.com <yves.lauber@varian.com>
2013-12-02 09:32:47 +0100

    files added

---
+++ src/programmierprojektYvesLauber/Observable.java
@@ -0,0 +1,6 @@
+package programmierprojektYvesLauber;
+
+public interface Observable {
+	void addObserver(Observer observer);
+	void removeObserver(Observer observer);
+}
---
+++ src/programmierprojektYvesLauber/Observer.java
@@ -0,0 +1,5 @@
+package programmierprojektYvesLauber;
+
+public interface Observer {
+	void update(Observable model);
+}
